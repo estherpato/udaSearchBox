10487a897ed7a613bda61d7358f7b59eb818f898
Esther Pato <estherpato@gmail.com>
2018-09-06 18:25:36 +0200

    Create cadastre component

---
+++ src/components/Cadastre.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Cadastre extends Component {
+
+    render() {
+        const {
+            placeholder,
+        } = this.props
+        return (
+            <div>
+                {/* <label
+                    htmlFor="input-search"
+                    style={{ display: 'none' }}
+                >
+                    {placeholder}
+                </label>
+                <input id='urbanTourPlaces'
+                    placeholder={placeholder}
+                    ref={(input) => { this.autoCompletePlace = input; }} /> */}
+            </div>
+        );
+    }
+}
+
+export default Cadastre;
