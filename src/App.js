f593960130ff520bedd9bde3838ae2130cfbe915
Loreto Vaquero <loret.vaquero@gmail.com>
2018-09-06 19:36:42 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Places from 'places.js';
 
 class App extends Component {
+
+  componentDidMount() {
+
+    let options = {
+      container: this.autoCompletePlace,
+      language: this.props.language,
+      useDeviceLocation: this.props.useDeviceLocation,
+
+    };
+
+    const optionnalPropsKeys = ['type', 'countries', 'aroundLatLng', 'aroundRadius', 'templates', 'appId', 'apiKey', 'apiKey'];
+
+    for (let optionnalPropKey of optionnalPropsKeys) {
+      if (this.props[optionnalPropKey]) { options[optionnalPropKey] = this.props[optionnalPropKey];}
+    }
+
+    const autocomplete = place(options);
+    autocomplete.on('change', this.props.onChange);
+    autocomplete.on('suggestions', this.props.onSuggestions);
+    autocomplete.on('clear', this.props.onClear);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <input id='urbanTourPlaces'
+        placeholder={this.props.placeholder}
+        disabled={this.props.disabled}
+        ref={(input) => {this.autoCompletePlace = input;}}
+      />
     );
   }
 }
+
+Places.propTypes = {
+  placeholder: PropTypes.string,
+  apiKey: PropTypes.string,
+  appId: PropTypes.string,
+  aroundLatLng: PropTypes.string,
+  aroundRadius: PropTypes.number,
+  countries: PropTypes.arrayOf(PropTypes.string),
+  disabled: PropTypes.bool,
+  language: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  onSuggestions: PropTypes.func.isRequired,
+  onClear: PropTypes.func.isRequired,
+  templates: PropTypes.object,
+  type: PropTypes.oneOf(['city', 'country', 'address', 'busStop', 'trainStation', 'townhall', 'airport']),
+  useDeviceLocation: PropTypes.bool,
+};
+
+Places.defaultProps = {
+  disabled: false,
+  language: navigator.language,
+  useDeviceLocation: false,
+  onChange: (query, rawAnswer, suggestion, suggestionIndex) => console.log(query, rawAnswer, suggestion, suggestionIndex),
+  onSuggestions: (query, rawAnswer, suggestions) => console.log(query, rawAnswer, suggestions),
+  onClear: (query) => console.log(query),
+};
 
 export default App;
