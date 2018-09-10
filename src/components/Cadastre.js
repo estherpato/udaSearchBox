7388e7a9cfe5501b0a0f8e9d586199ad30b58087
Loreto Vaquero <loret.vaquero@gmail.com>
2018-09-10 20:56:44 +0200

    Cadastre WIP

--- src/components/Cadastre.js
+++ src/components/Cadastre.js
@@ -1,6 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const url ='http://geo.reds.urbandataanalytics.com/geocoder/api/v1.0/cadastre/8925602VK3782F0012JB';
+
 
 class Cadastre extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      refCadastre: null,
+      latlng: null,
+    }
+  }
+
+  componentDidMount() {
+    axios({
+    url: url,
+    method: 'get',
+    auth: {
+            username: 'adalab',
+            password: '4286'
+        }
+    // headers: {
+    //     'Authorization': 'ba800d5f-ccb0-4e6e-91ed-7072ceeec58f'
+    // }
+ })
+      .then(res => {
+        const feature = res.data;
+        console.log(url);
+        console.log(feature);
+        // this.setState({
+        //   latlng: feature
+        // });
+        console.log(this.state.latlng);
+      })
+    }
 
     render() {
         return (
