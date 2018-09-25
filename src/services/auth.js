767e2055457aca5faa9e85695b522fbe7a9fe9b4
rafaelfernandezgis <rafael.fernandez.gis@gmail.com>
2018-09-25 08:03:16 +0200

    Handing errors and user info

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,4 @@
-import request from 'axios';
+import axios from 'axios';
 
 export function getToken(user, pwd) {
     const reports = {
@@ -8,12 +8,14 @@ export function getToken(user, pwd) {
     };
 
     return new Promise((resolve, reject) => {
-        request.post(reports.url, reports.data, { headers: reports.headers })
+        axios.post(reports.url, reports.data, { headers: reports.headers })
             .then(res => {
                 resolve(res)
             })
-            .catch(e => {
-                resolve(e.response)
-            })
+            .catch(function (e) {
+                let error = new Error('Bad credentials. This component will works but you still will need a valid token for most uDA components. Please contact support@urbandataanalytics.com to get your token.', e);
+                reject(error);
+              });
+        
     })
 }
